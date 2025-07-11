23cd0fff3417fbac02e368f04761d7c850093ef9
Ian <mr.bowtie@Ians-MacBook-Pro.local>
2025-07-10 23:46:41 -0400

    chore: add locations scaffolding

--- test/factories/inventory/locations.rb
+++ test/factories/inventory/locations.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: inventory_locations
+#
+#  id         :bigint           not null, primary key
+#  label      :string
+#  position   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :inventory_location, class: 'Inventory::Location' do
     
--- test/models/inventory/location_test.rb
+++ test/models/inventory/location_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: inventory_locations
+#
+#  id         :bigint           not null, primary key
+#  label      :string
+#  position   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class Inventory::LocationTest < ActiveSupport::TestCase
