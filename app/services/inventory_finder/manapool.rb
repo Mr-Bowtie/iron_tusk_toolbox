85593b1c405e8115977099945705f0491427a2f0
Ian <mr.bowtie@Ians-MacBook-Pro.local>
2025-08-30 15:56:55 -0400

    refactor: pull scryfall id from metadata instead of card directly
    allows pulling items that were added manually since they are attached to
    their metadata id instead of getting a scryfall id passed from a CSV

--- app/services/inventory_finder/manapool.rb
+++ app/services/inventory_finder/manapool.rb
@@ -4,21 +4,25 @@ module InventoryFinder
     #
     # @param row [CSV::Row]
     def self.find_from_csv(row)
-      Inventory::Card.joins(:inventory_location).where(
-          tcgplayer: false,
-          scryfall_id: row["scryfall_id"],
-          condition: Inventory::Card.map_condition(row["condition"]),
-          foil: Inventory::Card.map_foil(row["foil"])
-        ).order("inventory_locations.label ASC").to_a
+      Inventory::Card.joins(:metadata, :inventory_location).where(
+        tcgplayer: false,
+        condition: Inventory::Card.map_condition(row["condition"]),
+        foil: Inventory::Card.map_foil(row["foil"]),
+        metadata: {
+          scryfall_id: row["scryfall_id"]
+        }
+      ).order("inventory_locations.label ASC").to_a
     end
 
     def self.find_from_order_item(item)
-      Inventory::Card.joins(:inventory_location).where(
-              tcgplayer: false,
-              scryfall_id: item["scryfall_id"],
-              condition: Inventory::Card.map_condition(item["condition_id"]),
-              foil: Inventory::Card.map_foil(item["finish_id"])
-            ).order("inventory_locations.label ASC").to_a
+      Inventory::Card.joins(:metadata, :inventory_location).where(
+        tcgplayer: false,
+        condition: Inventory::Card.map_condition(item["condition_id"]),
+        foil: Inventory::Card.map_foil(item["finish_id"]),
+        metadata: {
+          scryfall_id: item["scryfall_id"]
+        }
+      ).order("inventory_locations.label ASC").to_a
     end
   end
 end
