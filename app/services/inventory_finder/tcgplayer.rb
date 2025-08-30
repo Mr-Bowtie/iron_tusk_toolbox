a3c386874e7b36425102e2bca51d06d15b974238
Ian <mr.bowtie@Ians-MacBook-Pro.local>
2025-08-30 16:06:59 -0400

    fix: tcgplayer commander set names not matching scryfall

--- app/services/inventory_finder/tcgplayer.rb
+++ app/services/inventory_finder/tcgplayer.rb
@@ -7,9 +7,19 @@ module InventoryFinder
         foil: row["Condition"].include?("Foil"),
         metadata: {
           collector_number: row["Number"],
-          set_name: row["Set"]
+          set_name: set_name_converter(row["Set"])
         }
       ).order("inventory_locations.label ASC").to_a
+    end
+
+    private
+
+    def set_name_converter(name)
+      if name.include?("Commander:")
+        name = name.gsub("Commander: ", "") + " Commander"
+      end
+
+      name
     end
   end
 end
