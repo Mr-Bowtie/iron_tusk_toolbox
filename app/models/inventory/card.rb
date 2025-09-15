085c0971df6af38748fea764749cd9d824843027
Ian <mr.bowtie@Ians-MacBook-Pro.local>
2025-09-15 12:51:33 -0400

    fix: foil checker for manabox csv

--- app/models/inventory/card.rb
+++ app/models/inventory/card.rb
@@ -86,7 +86,7 @@ class Inventory::Card < ApplicationRecord
   # maps foil input from field in ingested csv to internal boolean
   def self.map_foil(foil)
     case foil
-    when "FO", /^.*Foil$/, "EF", "etched"
+    when "FO", /^.*Foil$/, "EF", "etched", "foil"
       true
     else
       false
